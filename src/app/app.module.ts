ef0a4ce6ac3b32928c1383febb3754eaba101fe2
tatchiwiggers <tatisabato@hotmail.com>
2022-09-11 23:05:04 -0300

    adds home page and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,26 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipes/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: 'recipes', component: RecipeListComponent},
-      {path: 'recipes/:id', component: RecipeListComponent}
+      { path: 'recipes', component: RecipeListComponent },
+      { path: 'recipes/:id', component: RecipeDetailComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch:'full' },
+      { path: '**', redirectTo: 'home', pathMatch:'full' }
     ])
   ],
   providers: [],
