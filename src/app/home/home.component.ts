d96b900fcb093561f4c008ceae6fab91df37c385
tatchiwiggers <tatisabato@hotmail.com>
2022-09-11 23:06:45 -0300

    adds home page and routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,21 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-<<<<<<< HEAD
   
   pageTitle:string = 'Home'
 
-=======
-  pageTitle: string = "Home"
-  
->>>>>>> master
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> master
